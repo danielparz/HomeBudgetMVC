b8935765a5f944b2d06cdeb78da8d26e1bfb40b3
danielparz <dparzynski@gmail.com>
2021-02-26 14:53:24 +0100

    Add and configure db Context

---
+++ HomeBudgetMVC.Infrastructure/Context.cs
@@ -0,0 +1,34 @@
+﻿using HomeBudgetMVC.Domain.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeBudgetMVC.Infrastructure
+{
+    public class Context : IdentityDbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Subcategory> Subcategories { get; set; }
+
+        public Context(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Item>()
+                .HasOne(it => it.Subcategory)
+                .WithMany(sub => sub.Items)
+                .HasForeignKey(fk => fk.SubcategoryId);
+            builder.Entity<Subcategory>()
+                .HasOne(a => a.Category)
+                .WithMany(b => b.Subcategories)
+                .HasForeignKey(fk => fk.CategoryId);
+        }
+    }
+}
--- HomeBudgetMVC.Web/Startup.cs
+++ HomeBudgetMVC.Web/Startup.cs
@@ -3,15 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
-using HomeBudgetMVC.Web.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using HomeBudgetMVC.Infrastructure;
+using Microsoft.AspNetCore.Identity;
 
 namespace HomeBudgetMVC.Web
 {
@@ -27,11 +26,11 @@ namespace HomeBudgetMVC.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
+            services.AddDbContext<Context>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+                .AddEntityFrameworkStores<Context>();
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
